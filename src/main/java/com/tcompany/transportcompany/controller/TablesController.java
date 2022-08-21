661ca7da43cae3afdcf966856fded1a1e9eee38b
Moryee <alex.fedorutsa@gmail.com>
2022-08-21 16:10:57 +0300

    added logging, fixed bug with deleteTrailer controller

--- src/main/java/com/tcompany/transportcompany/controller/TablesController.java
+++ src/main/java/com/tcompany/transportcompany/controller/TablesController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @RestController
@@ -37,21 +38,28 @@ public class TablesController {
     @Autowired
     private TrailersRepository trailersRepository;
 
+    private void logInfo(String method, String tableName) {
+        log.info("TablesController: " + method.toUpperCase(Locale.ROOT) + " " + tableName);
+    }
+
     // Cargo
     @GetMapping(path = CargoLinks.GET_CARGO)
     public ResponseEntity<?> getCargo() {
+        logInfo("get", "cargo");
         List<Cargo> resource = cargoRepository.findAll();
         return ResponseEntity.ok(resource);
     }
 
     @PostMapping(path = CargoLinks.POST_CARGO)
     public ResponseEntity<?> postCargo(@RequestBody Cargo cargo) {
+        logInfo("post", "cargo");
         Cargo resource = cargoRepository.insert(cargo);
         return ResponseEntity.ok(resource);
     }
 
     @PutMapping(path = CargoLinks.PUT_CARGO)
     public ResponseEntity<?> putCargo(@RequestBody Cargo newCargo) {
+        logInfo("put", "cargo");
         Cargo cargo = cargoRepository.findById(newCargo.getId())
                 .orElseThrow(() -> new RuntimeException(
                     String.format("Cannot Find Cargo by ID %s", newCargo.getId())));
@@ -67,6 +75,7 @@ public class TablesController {
 
     @DeleteMapping(path = CargoLinks.DELETE_CARGO)
     public ResponseEntity<?> deleteCargo(@RequestBody Cargo cargo) {
+        logInfo("delete", "cargo");
         String id = cargo.getId();
         cargoRepository.deleteById(id);
         return ResponseEntity.ok(id);
@@ -75,18 +84,21 @@ public class TablesController {
     // Drivers
     @GetMapping(path = DriversLinks.GET_DRIVERS)
     public ResponseEntity<?> getDrivers() {
+        logInfo("get", "drivers");
         List<Drivers> resource = driversRepository.findAll();
         return ResponseEntity.ok(resource);
     }
 
     @PostMapping(path = DriversLinks.POST_DRIVER)
     public ResponseEntity<?> postDriver(@RequestBody Drivers driver) {
+        logInfo("post", "drivers");
         Drivers resource = driversRepository.insert(driver);
         return ResponseEntity.ok(resource);
     }
 
     @PutMapping(path = DriversLinks.PUT_DRIVER)
     public ResponseEntity<?> putDriver(@RequestBody Drivers newDriver) {
+        logInfo("put", "drivers");
         Drivers driver = driversRepository.findById(newDriver.getId())
                 .orElseThrow(() -> new RuntimeException(
                         String.format("Cannot Find Driver by ID %s", newDriver.getId())));
@@ -100,6 +112,7 @@ public class TablesController {
 
     @DeleteMapping(path = DriversLinks.DELETE_DRIVER)
     public ResponseEntity<?> deleteDriver(@RequestBody Drivers driver) {
+        logInfo("delete", "drivers");
         String id = driver.getId();
         driversRepository.deleteById(id);
 
@@ -109,18 +122,21 @@ public class TablesController {
     // Trucks
     @GetMapping(path = TrucksLinks.GET_TRUCKS)
     public ResponseEntity<?> getTrucks() {
+        logInfo("get", "trucks");
         List<Trucks> resource = trucksRepository.findAll();
         return ResponseEntity.ok(resource);
     }
 
     @PostMapping(path = TrucksLinks.POST_TRUCK)
     public ResponseEntity<?> postTruck(@RequestBody Trucks truck) {
+        logInfo("post", "trucks");
         Trucks resource = trucksRepository.insert(truck);
         return ResponseEntity.ok(resource);
     }
 
     @PutMapping(path = TrucksLinks.PUT_TRUCK)
     public ResponseEntity<?> putTruck(@RequestBody Trucks newTruck) {
+        logInfo("put", "trucks");
         Trucks truck = trucksRepository.findById(newTruck.getId())
                 .orElseThrow(() -> new RuntimeException(
                         String.format("Cannot Find Truck by ID %s", newTruck.getId())));
@@ -135,6 +151,7 @@ public class TablesController {
 
     @DeleteMapping(path = TrucksLinks.DELETE_TRUCK)
     public ResponseEntity<?> deleteTruck(@RequestBody Trucks truck) {
+        logInfo("delete", "trucks");
         String id = truck.getId();
         trucksRepository.deleteById(id);
         return ResponseEntity.ok(id);
@@ -143,18 +160,21 @@ public class TablesController {
     // Trailers
     @GetMapping(path = TrailersLinks.GET_TRAILERS)
     public ResponseEntity<?> getTrailers() {
+        logInfo("get", "trailers");
         List<Trailers> resource = trailersRepository.findAll();
         return ResponseEntity.ok(resource);
     }
 
     @PostMapping(path = TrailersLinks.POST_TRAILER)
     public ResponseEntity<?> postTrailer(@RequestBody Trailers trailer) {
+        logInfo("post", "trailers");
         Trailers resource = trailersRepository.insert(trailer);
         return ResponseEntity.ok(resource);
     }
 
     @PutMapping(path = TrailersLinks.PUT_TRAILER)
     public ResponseEntity<?> putTrailer(@RequestBody Trailers newTrailer) {
+        logInfo("put", "trailers");
         Trailers trailer = trailersRepository.findById(newTrailer.getId())
                 .orElseThrow(() -> new RuntimeException(
                         String.format("Cannot Find Trailer by ID %s", newTrailer.getId())));
@@ -168,7 +188,8 @@ public class TablesController {
     }
 
     @DeleteMapping(path = TrailersLinks.DELETE_TRAILER)
-    public ResponseEntity<?> deleteTruck(@RequestBody Trailers trailer) {
+    public ResponseEntity<?> deleteTrailer(@RequestBody Trailers trailer) {
+        logInfo("delete", "trailers");
         String id = trailer.getId();
         trailersRepository.deleteById(id);
         return ResponseEntity.ok(id);
