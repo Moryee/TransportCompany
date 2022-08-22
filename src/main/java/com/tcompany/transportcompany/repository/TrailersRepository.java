1bc8301cbe402a92667c4458e3500c77f50e93eb
Moryee <alex.fedorutsa@gmail.com>
2022-08-22 12:14:35 +0300

    added constraints onDelete and onPost

--- src/main/java/com/tcompany/transportcompany/repository/TrailersRepository.java
+++ src/main/java/com/tcompany/transportcompany/repository/TrailersRepository.java
@@ -3,8 +3,18 @@ package com.tcompany.transportcompany.repository;
 
 import com.tcompany.transportcompany.entity.Trailers;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface TrailersRepository extends MongoRepository<Trailers, String> {}
+public interface TrailersRepository extends MongoRepository<Trailers, String> {
+
+    @Query("{'cargo': ?0}")
+    List<Trailers> findByCargoId(String cargoId);
+
+    @Query("{'truck': ?0}")
+    List<Trailers> findByTruckId(String truckId);
+}
--- src/main/java/com/tcompany/transportcompany/repository/TrucksRepository.java
+++ src/main/java/com/tcompany/transportcompany/repository/TrucksRepository.java
@@ -1,10 +1,19 @@
 package com.tcompany.transportcompany.repository;
 
 
+import com.tcompany.transportcompany.entity.Trailers;
 import com.tcompany.transportcompany.entity.Trucks;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface TrucksRepository extends MongoRepository<Trucks, String> {}
+public interface TrucksRepository extends MongoRepository<Trucks, String> {
+
+    @Query("{'driver': ?0}")
+    List<Trucks> findByDriverId(String driverId);
+
+}
