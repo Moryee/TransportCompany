a079f1a29fed701f2ace9c2b9e38b6b89b9d6730
Moryee <alex.fedorutsa@gmail.com>
2022-09-02 23:44:38 +0300

    added select inputs to some forms

--- src/main/ui/src/pages/forms/TrailerForm.js
+++ src/main/ui/src/pages/forms/TrailerForm.js
@@ -3,6 +3,7 @@ import { Grid, } from '@mui/material';
 import { Button, Checkbox, Input, RadioGroup, Select } from '../../components/controls';
 import { useForm, Form } from '../../components/useForm';
 
+import { getData } from '../../service/service';
 
 export const initialValues = {
     id: 0,
@@ -13,6 +14,47 @@ export const initialValues = {
 
 export default function TruckForm(props) {
     const { addOrEdit, recordForEdit } = props
+    const [trucks, setTrucks] = useState([])
+    const [trailers, setTrailers] = useState([])
+    const [cargo, setCargo] = useState([])
+    const [availableTrucks, setAvailableTrucks] = useState([])
+    const [availableCargo, setAvailableCargo] = useState([])
+
+    useEffect(() => {
+        getData(setTrucks, 'trucks')
+        getData(setTrailers, 'trailers')
+        getData(setCargo, 'cargo')
+    }, [])
+
+    // select items for trucks
+    useEffect(() => {
+        let result = []
+        trucks.forEach(truck => {
+            if(!trailers.some(trailer => trailer.truck == truck.id)) {
+                const newDict = {
+                    id: truck.id,
+                    title: `${truck.model}`,
+                }
+                result.push(newDict)
+            }
+        });
+        setAvailableTrucks(result)
+    }, [trailers, trucks])
+
+    // select items for cargo
+    useEffect(() => {
+        let result = []
+        cargo.forEach(crg => {
+            if(!trailers.some(trailer => trailer.cargo == crg.id)) {
+                const newDict = {
+                    id: crg.id,
+                    title: `${crg.type}`,
+                }
+                result.push(newDict)
+            }
+        });
+        setAvailableCargo(result)
+    }, [cargo])
 
     const validate = (fieldValues = values) => {
         let temp = { ...errors }
@@ -49,6 +91,7 @@ export default function TruckForm(props) {
 
     useEffect(() => {
         if (recordForEdit != null)
+            console.log(recordForEdit)
             setValues({
                 ...recordForEdit
             })
@@ -60,19 +103,20 @@ export default function TruckForm(props) {
                 direction={'column'}
                 rowGap={2}
                 sx={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px', paddingBottom: '20px' }}>
-
-                <Input
+                <Select
                     name="truck"
                     label="Truck"
                     value={values.truck}
                     onChange={handleInputChange}
+                    options={availableTrucks}
                     error={errors.truck}
                 />
-                <Input
+                <Select
                     name="cargo"
                     label="Cargo"
                     value={values.cargo}
                     onChange={handleInputChange}
+                    options={availableCargo}
                     error={errors.cargo}
                 />
                 <Input
