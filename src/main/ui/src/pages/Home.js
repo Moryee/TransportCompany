72b631432c95fef051f31e6e2c5e530310c09099
Sasha <110664032+YgrOl@users.noreply.github.com>
2022-09-10 00:38:25 +0300

    Text design on the Home page
    Дизайн тексту Home page

--- src/main/ui/src/pages/Home.js
+++ src/main/ui/src/pages/Home.js
@@ -3,11 +3,12 @@ import React from 'react';
 function Home() {
     return (
         <div className='home-page'>
-            {/* <div className='text-box'>
+            { <div className='text-box'>
                 <h1 className='text1'>ПЕРЕВЕЗЕННЯ ЧОГО ЗАВГОДНО</h1>
-                <h1 className='text2'>І КУДИ ЗАВГОДНО</h1>
-                <p className='text3'>БІЛЬШЕ 10 ТИСЯЧ ПЕРЕВЕЗЕНЬ УКРАЇНОЮ ТА ЄВРОПОЮ</p>
-            </div> */}
+                <h1 className='text2'>КУДИ ЗАВГОДНО</h1>
+                <h1 className='text3'>КОЛИ ЗАВГОДНО</h1>
+                <p className='text4'>БІЛЬШЕ 10 ТИСЯЧ УСПІШНИХ ПЕРЕВЕЗЕНЬ УКРАЇНОЮ ТА КРАЇНАМИ ЄВРОПИ</p>
+            </div> }
         </div>
     );
 }
